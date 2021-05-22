65f83e8ffe252bab3b9a515db4948296931caf77
askabdulhaseeb <askabdulhaseeb@gmail.com>
2021-05-23 03:53:41 +0500

    Connect with firebase Android

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,9 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:inventory_app/screens/addProductScreen/add_product_screen.dart';
-import 'package:inventory_app/screens/loginScreen/login_screen.dart';
-import 'package:inventory_app/screens/searchScreen/search_screen.dart';
-
 import 'database/user_local_data.dart';
+import 'screens/addProductScreen/add_product_screen.dart';
+import 'screens/loginScreen/login_screen.dart';
+import 'screens/searchScreen/search_screen.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -14,11 +13,10 @@ Future<void> main() async {
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Inventory System',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
@@ -31,3 +29,7 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
+//  Certificate fingerprints:
+//    SHA1: F6:3C:6E:07:37:98:D1:37:8D:8D:AD:2B:80:BE:5E:2C:50:EF:71:F9
+//    SHA256: B8:1F:B2:FF:CB:2E:A2:45:12:1B:22:43:35:C5:B6:CC:A5:3B:CE:D4:6B:97:93:EF:76:D9:81:0F:F9:16:4C:6E
